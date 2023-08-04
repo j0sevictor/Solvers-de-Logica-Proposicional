aaebc135562616352ea9ed97e97526333d4c39fc
José Victor <56090571+ze-123@users.noreply.github.com>
2023-08-04 11:51:53 -0300

    leitura do arquivo

--- DPLL/dpll.py
+++ DPLL/dpll.py
@@ -1,8 +1,32 @@
 def main():
-    print('oi')
+    clauses, numVariaveis, numClausues = cnfFormula()
+    print(clauses, numVariaveis, numClausues)
 
 
+def cnfFormula() -> tuple:
+    numVariables, numClauses = 0, 0
 
+    with open('DPLL\CNFFormula.txt', 'r') as formulae:
+        descStr = formulae.readline().split(sep=' ')
+        numVariables, numClauses = int(descStr[-2]), int(descStr[-1])
+
+        clauses = list()
+
+        lines = formulae.readlines()
+        for line in lines:
+            clause = set()
+
+            for var in  line.split(sep=' '):
+                if '0' in var:
+                    break
+                clause.add(int(var))
+
+            clauses.append(clause)
+    
+    return (clauses, numVariables, numClauses)
+
+def simplifica(cl):
+    pass
 
 if __name__ == '__main__':
     main()
